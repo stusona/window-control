53752aa3b3299700e5560f51a42e98db85949c17
stusona <supadupastu@gmail.com>
2017-11-07 22:35:19 -0800

    window-demo ready for bluetooth stuff

--- stepper-demo.ino
+++ stepper-demo.ino
@@ -1,11 +1,11 @@
-const int stepPin = 3; 
-const int dirPin = 4; 
+const int stepPin = 3;
+const int dirPin = 4;
 const int sleepPin = 5;
-const int period = 950; 
+const int period = 1500;
 
 void setup() {
   // Sets the two pins as Outputs
-  pinMode(stepPin,OUTPUT); 
+  pinMode(stepPin,OUTPUT);
   pinMode(dirPin,OUTPUT);
   pinMode(sleepPin,OUTPUT);
 }
@@ -13,17 +13,17 @@ void setup() {
 void loop() {
   // enable driver and set direction
   digitalWrite(sleepPin, HIGH);
-  digitalWrite(dirPin,HIGH); 
-  
-  // 200 pulses is one full cycle 
+  digitalWrite(dirPin,HIGH);
+
+  // 200 pulses is one full cycle
   for(int x = 0; x < 2000; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(period); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(period); 
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(period);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(period);
   }
 
   digitalWrite(sleepPin, LOW);
-  
+
   delay(3000); // One second delay
 }
--- window-demo.ino
+++ window-demo.ino
@@ -3,48 +3,86 @@ Motor control demo for window in Aurai system
 By Stuart Sonatina and Jacquie Nguyen
 IDD - 209U, UC Berkeley, November 2017
 Details:
-  Initialize window position by closing it until the reed switch is triggered
-  Move window to setpoint position by driving NEMA17 Stepper Motor
+  To be used with Feather Board
+  Waits for input from bluetooth, then opens or closes depending on input.
 
   Uses DRV8825 pololu stepper driver
  */
-#include <Arduino.h>
-#include "DRV8825.h"
 
 // Pin Definitions
 #define STEP_PIN    5
 #define DIR_PIN     6
 #define SLEEP_PIN   9  // HIGH enables driver and LOW puts it to sleep
-#define POT_PIN     A0 // Sense voltage on Potentiometer wiper
-#define REED_PIN    A1 // Sense reed switch (LOW is open and HIGH is closed)
+//#define POT_PIN     A0 // Sense voltage on Potentiometer wiper
+//#define REED_PIN    A1 // Sense reed switch (LOW is open and HIGH is closed)
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-#define MOTOR_STEPS 200
-#define RPM 16
-#define MICROSTEPS 1  // 1=full step, 2=half step etc.
+#define STEPS_PER_REV 200
+#define RPM 10
+#define PERIOD 3000; // microseconds
 
-DRV8825 stepper(MOTOR_STEPS, DIR_PIN, STEP_PIN);
+bool characteristic;
 
 void setup() {
-  // Initialize stepper
-  stepper.begin(RPM, MICROSTEPS);
-
-  // Initialize pins
+  // Initialize pis
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
   pinMode(SLEEP_PIN,OUTPUT);
+
+  // Make sure stepper driver is off
+  digitalWrite(SLEEP_PIN, LOW);
 }
 
 void loop() {
 
-  // Enable stepper driver
-  digitalWrite(SLEEP_PIN,HIGH);
+  switch (characteristic) {
+    case 0: // open window
+    {
+      // Enable stepper driver and set direction
+      digitalWrite(SLEEP_PIN,HIGH);
+      digitalWrite(dirPin,HIGH);
 
-  /** Move motor using steps since each step is 1.8
-  * Positive to move forward, negative to reverse
-  */
-  stepper.move(200);
+      // turn 10 times
+      for(int x = 0; x < STEPS_PER_REV*10; x++) {
+        digitalWrite(stepPin,HIGH);
+        delayMicroseconds(PERIOD/2);
+        digitalWrite(stepPin,LOW);
+        delayMicroseconds(PERIOD/2);
+      }
+
+      // Turn stepper driver off
+      digitalWrite(SLEEP_PIN, LOW);
+
+      break;
+    }
+
+    case 1: // close window
+    {
+      // Enable stepper driver and set direction
+      digitalWrite(SLEEP_PIN,HIGH);
+      digitalWrite(dirPin,LOW);
+
+      // turn 10 times
+      for(int x = 0; x < STEPS_PER_REV*10; x++) {
+        digitalWrite(stepPin,HIGH);
+        delayMicroseconds(PERIOD/2);
+        digitalWrite(stepPin,LOW);
+        delayMicroseconds(PERIOD/2);
+      }
+
+      // Turn stepper driver off
+      digitalWrite(SLEEP_PIN, LOW);
+
+      break;
+    }
+
+    default:
+    {
+      // Make sure stepper driver is off
+      digitalWrite(SLEEP_PIN, LOW);
+    }
+  }
 
   // Disable stepper driver to allow for manual movement
   digitalWrite(SLEEP_PIN,LOW);
-
-  delay(3000);
 }
