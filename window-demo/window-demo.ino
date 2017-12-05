15e2b64bafd1dd1207f3e8b54007043d48d8c58b
stusona <supadupastu@gmail.com>
2017-12-05 15:26:50 -0800

    It works! Changed mapping, timeout, error, etc

--- window-demo/window-demo.ino
+++ window-demo/window-demo.ino
@@ -23,6 +23,8 @@
   #include <SoftwareSerial.h>
 #endif
 
+// want bluetooth?
+#define BT_ENB false
 
 // Pin Definitions
 #define PWM_PIN     5  // Enable pin on Motor Driver
@@ -31,15 +33,25 @@
 #define POT_PIN     A0 // Sense voltage on Potentiometer wiper
 #define REED_PIN    A1 // Sense reed switch (LOW is open and HIGH is closed)
 
-
 // Number of milliseconds before moveWindow times out
-#define TIMEOUT   10000 // ms
+#define TIMEOUT   40000 // ms
 
 // Constants
-#define KP            -10
-#define MAX_ERROR     2   // allowable steady state error (percent)
+#define KP            30
+#define MAX_ERROR     0// allowable steady state error (percent)
 #define REED_OFFSET   2   // Amount window needs to move past triggering
                           // of reed switch (percent)
+
+// Define potentiometer scale (can be between 0 and 1023)
+// This needs to be calibrated. Let's start with a small range in the middle.
+float pot_open  = 1015; // adc counts
+float pot_close = 0;    // adc counts
+float pot_scale = 100.0/(pot_open - pot_close); // percent / adc counts
+
+//Initialize window control variables
+int32_t pos_current = 0;
+int32_t pos_setpoint = 0;
+boolean valueChanged = false;
 
 /** Create the bluefruit object
  * Hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user-selected
@@ -57,17 +69,6 @@ void error(const __FlashStringHelper*err) {
 int32_t customServiceID;
 int32_t positionCharID;
 int32_t setpointCharID;
-
-// Define potentiometer scale (can be between 0 and 1023)
-// This needs to be calibrated. Let's start with a small range in the middle.
-float pot_open  = 1023; // adc counts
-float pot_close = 0;    // adc counts
-float pot_scale = 100.0/(pot_open - pot_close); // percent / adc counts
-
-//Initialize window control variables
-int32_t pos_current = 0;
-int32_t pos_setpoint = 0;
-boolean valueChanged = false;
 
 
 /**************************************************************************/
@@ -98,86 +99,88 @@ void setup(void)
   // *call that position "pot_close"
 
 
-  //////////////////////// BEGIN ADAFRUIT BLUETOOTH CODE ///////////////////////
-  // Set up the BLE module
-  while (!Serial); // required for Flora & Micro
-  delay(500);
+  if(BT_ENB) {
+    //////////////////////// BEGIN ADAFRUIT BLUETOOTH CODE ///////////////////////
+    // Set up the BLE module
+    while (!Serial); // required for Flora & Micro
+    delay(500);
 
-  boolean success;
+    boolean success;
 
-  Serial.println(F("Adafruit Bluefruit Custom AURAI Window Module"));
-  Serial.println(F("---------------------------------------------------"));
+    Serial.println(F("Adafruit Bluefruit Custom AURAI Window Module"));
+    Serial.println(F("---------------------------------------------------"));
 
-  randomSeed(micros());
+    randomSeed(micros());
 
-  /* Initialise the module */
-  Serial.print(F("Initialising the Bluefruit LE module: "));
+    /* Initialise the module */
+    Serial.print(F("Initialising the Bluefruit LE module: "));
 
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring"));
-  }
-  Serial.println( F("OK!") );
+    if ( !ble.begin(VERBOSE_MODE) )
+    {
+      error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring"));
+    }
+    Serial.println( F("OK!") );
 
-  /* Perform a factory reset to make sure everything is in a known state */
-  Serial.println(F("Performing a factory reset: "));
-  if (! ble.factoryReset() ){
-       error(F("Couldn't factory reset"));
-  }
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if (! ble.factoryReset() ){
+         error(F("Couldn't factory reset"));
+    }
 
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
+    /* Disable command echo from Bluefruit */
+    ble.echo(false);
 
-  Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
-  ble.info();
+    Serial.println("Requesting Bluefruit info:");
+    /* Print Bluefruit information */
+    ble.info();
 
-  // this line is particularly required for Flora, but is a good idea
-  // anyways for the super long lines ahead!
-  // ble.setInterCharWriteDelay(5); // 5 ms
+    // this line is particularly required for Flora, but is a good idea
+    // anyways for the super long lines ahead!
+    // ble.setInterCharWriteDelay(5); // 5 ms
 
-  /* Change the device name to make it easier to find */
-  Serial.println(F("Setting device name to 'Aurai Window Module': "));
+    /* Change the device name to make it easier to find */
+    Serial.println(F("Setting device name to 'Aurai Window Module': "));
 
-  if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Aurai Window Mod")) ) {
-    error(F("Could not set device name?"));
-  }
+    if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Aurai Window Mod")) ) {
+      error(F("Could not set device name?"));
+    }
 
 
-  /*Add custom service */
-    Serial.println(F("Adding the Custom Service definition "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID128=20-ff-77-b5-75-c2-45-b2-85-1d-b4-2b-04-1d-35-c3"), &customServiceID);
-  if (! success) {
-    error(F("Could not add custom service"));
-  }
-
-  /* Position characteristic */
-  Serial.println(F("Adding the position characteristic  "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=57-12-04-BC-B3-96-42-97-A5-13-31-1C-45-71-C0-23, PROPERTIES=0x02, MIN_LEN=1, MAX_LEN=4, VALUE=1000000000"), &positionCharID);
+    /*Add custom service */
+      Serial.println(F("Adding the Custom Service definition "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID128=20-ff-77-b5-75-c2-45-b2-85-1d-b4-2b-04-1d-35-c3"), &customServiceID);
     if (! success) {
-    error(F("Could not add position characteristic"));
+      error(F("Could not add custom service"));
+    }
+
+    /* Position characteristic */
+    Serial.println(F("Adding the position characteristic  "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=57-12-04-BC-B3-96-42-97-A5-13-31-1C-45-71-C0-23, PROPERTIES=0x02, MIN_LEN=1, MAX_LEN=4, VALUE=1000000000"), &positionCharID);
+      if (! success) {
+      error(F("Could not add position characteristic"));
+    }
+
+     /* Setpoint characteristic */
+    Serial.println(F("Adding the setpoint characteristic  "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=19-9b-42-78-8c-07-4e-71-bd-0d-7f-4e-1f-d5-76-d2, PROPERTIES=0x08, MIN_LEN=1, MAX_LEN=4, VALUE=1000000000"), &setpointCharID);
+      if (! success) {
+      error(F("Could not add setpoint characteristic"));
+    }
+
+    /* Add the Heart Rate Service to the advertising data (needed for Nordic apps to detect the service) */
+    Serial.print(F("Adding Custom Service UUID to the advertising payload: "));
+    //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-06-b5-77-0a-18") );
+    //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-05-02-b5-77-0a-18") );
+    //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18") );
+
+    /* Reset the device for the new service setting changes to take effect */
+    Serial.print(F("Performing a SW reset (service changes require a reset): "));
+    ble.reset();
+
+    Serial.println();
+
+    ////////////////////////// END ADAFRUIT BLUETOOTH CODE /////////////////////////
   }
-
-   /* Setpoint characteristic */
-  Serial.println(F("Adding the setpoint characteristic  "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=19-9b-42-78-8c-07-4e-71-bd-0d-7f-4e-1f-d5-76-d2, PROPERTIES=0x08, MIN_LEN=1, MAX_LEN=4, VALUE=1000000000"), &setpointCharID);
-    if (! success) {
-    error(F("Could not add setpoint characteristic"));
-  }
-
-  /* Add the Heart Rate Service to the advertising data (needed for Nordic apps to detect the service) */
-  Serial.print(F("Adding Custom Service UUID to the advertising payload: "));
-  //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-06-b5-77-0a-18") );
-  //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-05-02-b5-77-0a-18") );
-  //ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18") );
-
-  /* Reset the device for the new service setting changes to take effect */
-  Serial.print(F("Performing a SW reset (service changes require a reset): "));
-  ble.reset();
-
-  Serial.println();
-
-////////////////////////// END ADAFRUIT BLUETOOTH CODE /////////////////////////
 }
 
 
@@ -187,52 +190,48 @@ int incomingByte = 0;
 
 void loop(void)
 {
-  /* Command is sent when \n (\r) or println is called */
-  /* AT+GATTCHAR=CharacteristicID,value */
+  if(BT_ENB) {
+    /* Command is sent when \n (\r) or println is called */
+    /* AT+GATTCHAR=CharacteristicID,value */
 
-//  ble.sendCommandWithIntReply(F("AT+GATTCHAR=2"), &window_setpoint);
-//  Serial.print("window_setpoint2: ");
-//  Serial.println(window_setpoint);
+  //  ble.sendCommandWithIntReply(F("AT+GATTCHAR=2"), &window_setpoint);
+  //  Serial.print("window_setpoint2: ");
+  //  Serial.println(window_setpoint);
 
-  // Serial.println("----------***--------------");
-  // Print current position to serial monitor
-  pos_current = readPosition();
-  // Serial.print("Window is at: ");
-  // Serial.println(pos_current);
+    // send position to Pico
+    pos_current = readPosition();
+    if(BT_ENB) broadcastPosition();
 
-  // send position to Pico
-  broadcastPosition();
+    if (Serial.available() > 0) {
+      // read the incoming byte:
+       incomingByte = Serial.parseInt();
 
-  // Serial.println("----------------");
+       // say what you got:
+      //  Serial.print("I received: ");
+      //  Serial.println(incomingByte);
 
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-     incomingByte = Serial.parseInt();
+       pos_setpoint = incomingByte;
 
-     // say what you got:
-    //  Serial.print("I received: ");
-    //  Serial.println(incomingByte);
-
-     pos_setpoint = incomingByte;
-
-    //  Serial.print("pos_setpoint: ");
-    //  Serial.println(pos_setpoint);
-     ble.print( F("AT+GATTCHAR=") );
-     ble.print( setpointCharID );
-     ble.print(F(","));
-     ble.print(pos_setpoint, HEX);
-     ble.println( F("-00-00-00") );
-  }
-  else{
-    readSetpoint();
-  }
-
-  Serial.println("----------------");
+      //  Serial.print("pos_setpoint: ");
+      //  Serial.println(pos_setpoint);
+       ble.print( F("AT+GATTCHAR=") );
+       ble.print( setpointCharID );
+       ble.print(F(","));
+       ble.print(pos_setpoint, HEX);
+       ble.println( F("-00-00-00") );
+     }
+     else{
+       readSetpoint();
+     }
+   }
   // move the window to setpoint
-  moveWindow(pos_setpoint);
+  // moveWindow(pos_setpoint);
+
+  // move window to input integer
+  moveWindow(Serial.parseInt());
 
   // window should be at setpoint now. Send position to Pico.
-  broadcastPosition();
+  if(BT_ENB) broadcastPosition();
 
   /* Delay before next measurement update */
   delay(1000);
@@ -249,13 +248,21 @@ void loop(void)
  * Returns 0 upon success.
 ******************************************************************************/
 int moveWindow(int setpoint) {
+  
+  // Read current pot position
+  pos_current = readPosition();
+
+  // Check if input is in range (ignore 0 since that's the default value of parseInt)
+  if ((setpoint <= 0) || (setpoint > 100)) {
+    Serial.print("That position is not 1-100. ");
+    Serial.println("Remaining at current position.");
+    setpoint = pos_current;
+  }
 
   // Start a timer for the timeout
   unsigned long startTime = millis();
   unsigned long runTime = millis() - startTime;
 
-  // Read pot position and print to serial monitor
-  pos_current = readPosition();
   Serial.print("Move from ");
   Serial.print(pos_current);
   Serial.print(" to ");
@@ -302,16 +309,17 @@ int moveWindow(int setpoint) {
  *****************************************************************************/
 int readPosition() {
   // get position in adc counts 0-1023
-  pos_current = analogRead(POT_PIN);
+  int adcPosition = analogRead(POT_PIN);
 
   // If position is below min or above max, something is wrong
   if ((pos_current < pot_close) || (pos_current > pot_open)) {
     Serial.println("Window out of range");
   }
 
-  pos_current -= pot_close;   // subtract minimum from position
-  pos_current *= pot_scale;   // convert from adc counts to percent
+//  pos_current -= pot_close;   // subtract minimum from position
+//  pos_current *= pot_scale;   // convert from adc counts to percent
 
+  pos_current = map(adcPosition, pot_close, pot_open, 1, 100);
   Serial.print("Position Reading: ");
   Serial.print(pos_current);
   Serial.println(" percent");
@@ -389,4 +397,3 @@ float setMotor(float motorSpeed)
   // Return the actual controller output with saturation protection
   return motorSpeed;
 }
-
